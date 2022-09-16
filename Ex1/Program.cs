6216fc213d77a9730d147f973f52876486929420
SvetlanaB <bsv211112@gmail.com>
2022-09-16 15:41:12 +0300

    Ex1 added

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задание №1");
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+
+Console.WriteLine("max = " + a);
+
+else if (a < b)
+{
+    Console.WriteLine("max =" + b);
+}
+else if (a == b)
+{
+    Console.WriteLine("a = b");
+}
