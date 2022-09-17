82688dcbcc349206d06d392c45f9cd37d1052bb2
SvetlanaB <bsv211112@gmail.com>
2022-09-17 11:50:58 +0300

    Ex3 added

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задание №3");
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+
+Console.WriteLine("да");
+
+else if(number % 2 != 0)
+{
+    Console.WriteLine("нет");
+}
