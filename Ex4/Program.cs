44b6ac317972128cbae0ddd2b123a3bae98435ae
SvetlanaB <bsv211112@gmail.com>
2022-09-19 10:53:47 +0300

    ex 4 added

---
+++ Ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задание №4");
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = N - 1;
+
+while (count > 1)
+{
+    if (count % 2 == 0)
+
+        Console.WriteLine(count);
+        count = count - 1;
+    
+
+}
+Console.WriteLine("Конец");
